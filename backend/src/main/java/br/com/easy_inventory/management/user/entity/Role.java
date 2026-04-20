a9321b8bd0650e083532451394848b44624ad538
Guilherme\Guilherme <guilhermepllay157@gmail.com>
2026-04-20 15:26:15 -0300

    feat: add User entity, Role enum, and UserRepository

---
+++ backend/src/main/java/br/com/easy_inventory/management/user/entity/Role.java
@@ -0,0 +1,5 @@
+package br.com.easy_inventory.management.user.entity;
+
+public enum Role {
+    OWNER, EMPLOYEE
+}
---
+++ backend/src/main/java/br/com/easy_inventory/management/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package br.com.easy_inventory.management.user.repository;
+
+import br.com.easy_inventory.management.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
